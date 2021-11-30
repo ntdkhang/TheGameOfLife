60faaebcf6f0db52a2695810f4d31c742d0a2d40
Khang <khang.nguyen.trd@gmail.com>
2021-11-30 08:26:16 -0600

    Initial Commit

---
+++ TheGameOfLife/TheGameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGameOfLifeApp.swift
+//  TheGameOfLife
+//
+//  Created by Nguyen Tran Duy Khang on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct TheGameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
